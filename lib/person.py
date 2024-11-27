4fed8e4654d64733ec144a5cda32d159c3af5650
melrwa <orwamelkizedek@gmail.com>
2024-11-28 02:16:42 +0300

    complete lab

--- lib/person.py
+++ lib/person.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 APPROVED_JOBS = [
     "Admin",
     "Customer Service",
@@ -16,4 +14,32 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Unknown", job="Unknown"):
+        self._name = None  # Temporary attributes to avoid triggering validations
+        self._job = None
+        self.name = name
+        self.job = job
+
+    # Getter and setter for name
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value.title()  # Convert to title case
+
+    # Getter and setter for job
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = value
